d4f73f9faf09be12f9956f4287e4dc108420fa52
doinone@qq.com <doinone@qq.com>
2016-03-17 23:40:51 +0800

    14-Flask入门数据操作2:数据库操作命令

--- app/models.py
+++ app/models.py
@@ -4,11 +4,13 @@
 
 from . import db
 
+
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=True)
     users = db.relationship('User', backref='role')
+
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -16,3 +18,45 @@ class User(db.Model):
     name = db.Column(db.String, nullable=True)
     password = db.Column(db.String, nullable=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+
+
+
+'''
+
+#python manager.py shell
+
+>>> from app import db
+>>> from app import models
+>>> db.create_all()
+
+>>> from app.models import Role,User
+>>> admins=Role(name='administrators')
+>>> mod=Role(name='moderator')
+>>> db.session.add_all([admins,mod])
+>>> db.session.commit()
+
+>>> tim = User(name='Tim', role=admins)
+>>> db.session.add(tim)
+>>> db.session.commit()
+
+
+ tim.password = '123456'
+>>> db.session.add(tim)
+>>> db.session.commit()
+
+>>> db.session.delete(tim)
+>>> db.session.commit()
+
+>>> db.session.add(User(name='Tim', role=admins))
+>>> db.session.commit()
+
+>>> User.query.all()
+>>> User.query.get(1)
+
+>>> Role.query.get(1)
+>>> Role.query.get(2)
+>>> Role.query.get(3)
+>>> Role.query.get(2).name
+
+'''
