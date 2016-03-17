8203665c16e8e2c619521a32cb23bb2451e4ccf8
doinone@qq.com <doinone@qq.com>
2016-03-18 02:31:53 +0800

    15-数据库事件与数据迁移  安装数据迁移插件：#pip install flask-migrate 数据库初始化环境，生成相应的文件migrations夹里面的内容： #python manager.py db init 初始化的迁移，生成migrations/versions/里面的文件： #python manager.py db migrate -m 'Initial migration' 迁移生成数据库文件(删除以前的相关表)：#python manager.py db upgrade 迁移生成数据库文件(重新建立以前的相关表)：#python manager.py db downgrade

--- app/models.py
+++ app/models.py
@@ -11,6 +11,11 @@ class Role(db.Model):
     name = db.Column(db.String, nullable=True)
     users = db.relationship('User', backref='role')
 
+    @staticmethod
+    def seed():
+        db.session.add_all(map(lambda r: Role(name=r), ['Guests','Administrators']))
+        db.session.commit()
+
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -19,6 +24,11 @@ class User(db.Model):
     password = db.Column(db.String, nullable=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
+    @staticmethod
+    def on_created(target, value, initiator):
+        target.role = Role.query.filter_by(name='Guests') .first()
+
+db.event.listen(User.name, 'set', User.on_created)
 
 
 
@@ -59,4 +69,26 @@ class User(db.Model):
 >>> Role.query.get(3)
 >>> Role.query.get(2).name
 
+'''
+
+'''
+15-数据库事件与数据迁移
+ 安装数据迁移插件：#pip install flask-migrate
+数据库初始化环境，生成相应的文件migrations夹里面的内容： #python manager.py db init
+初始化的迁移，生成migrations/versions/里面的文件： #python manager.py db migrate -m 'Initial migration'
+迁移生成数据库文件(删除以前的相关表)：#python manager.py db upgrade
+迁移生成数据库文件(重新建立以前的相关表)：#python manager.py db downgrade
+
+应该有问题：
+执行下面命令只会删除以前的数据表
+#python manager.py db upgrade
+应该还要执行下面的命令，来生成相关的数据表：
+#python manager.py db downgrade
+那么​manage.py里面的deploy()方法也是有问题的，正确的是如下：
+@manager.command
+def deploy():
+    from app.models import Role
+    upgrade()
+    downgrade()
+    Role.seed()
 '''
--- manager.py
+++ manager.py
@@ -2,10 +2,14 @@
 
 from werkzeug.utils import secure_filename
 from flask.ext.script import Manager
-from app import create_app
+from app import create_app, db
+from flask_migrate import Migrate, MigrateCommand,upgrade,downgrade
 
 app = create_app()
 manager = Manager(app)
+
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 # 能及时debug信息，自动更新到浏览器上显示
 @manager.command
@@ -14,7 +18,7 @@ def dev():
     live_server = Server(app.wsgi_app)
     # 监控所有的文件
     live_server.watch('**/*.*')
-    live_server.serve(open_url=False)
+    live_server.serve(open_url=True)
 
 
 @manager.command
@@ -22,9 +26,13 @@ def test():
     pass
 
 
+# 数据迁移工作：其实是执行：migrations/versions里面的py文件
 @manager.command
 def deploy():
-    pass
+    from app.models import Role
+    upgrade()
+    downgrade()
+    Role.seed()
 
 if __name__ == '__main__':
     manager.run()
