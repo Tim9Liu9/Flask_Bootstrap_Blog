c312902bca4c4f752235e70832932654c6967d2b
doinone@qq.com <doinone@qq.com>
2016-03-20 14:22:34 +0800

    修改成gunicorn的环境是developer的sqlite。

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 from app import create_app
 
-application = create_app('production')
+# production   development
+application = create_app('development')
+print "flask blog app is starting ...-->"
 
 if __name__ == '__main__':
     application.run()
